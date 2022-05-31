1d2e0a7d169e9360a88a2e80310ebcc2bce1e9ad
Nick Spanos <nickspanosf1@gmail.com>
2022-05-31 23:50:53 +0300

    Change the activity card like the template of client and create one landpage (not ready)

---
+++ home1.php
@@ -0,0 +1,385 @@
+<?php
+$title = "Homeapage 1";
+include_once 'includes/header1.php';
+?>
+
+
+<section>
+        <div class="container margin_80_55 ">
+            <div class="row py-3">
+
+                <div class="col-lg-4 col-md-12 herotext py-4">
+                    <h4> Discover everything you need to know about your Vacation Experiences… </h4>
+                    <h3>Spend less money doing it more</h3>
+               
+                    <a href="adventure-detail.php" class="btn_1">How it works</a>
+
+                </div>
+
+
+                <div class="col-lg-8 col-md-12">
+                    <div class="row py-2   ">
+                        <div class="col-lg-4  d-md-none d-sm-none d-lg-block  "> <img src="assets/img/4.jpg" class="img-fluid herophoto " alt=""></div>
+                        <div class="col-lg-4 col-md-6 col-sm-6   col-xsm-6   overflow-visible  "> <img src="assets/img/2.jpg" class="img-fluid herophoto  overf" alt="m"></div>
+                        <div class="col-lg-4 col-md-6    col-sm-6  "> <img src="assets/img/bg_images/1.jpg" class="img-fluid herophoto" alt="m"></div>
+                    </div>
+
+                    <div class="row py-2 ">
+                        <div class="col-lg-4    "> <img src="assets/img/bg_images/2.jpg" class="img-fluid herophoto d-md-none d-sm-none d-lg-block" alt=""></div>
+                        <div class="col-lg-4  col-md-6   col-sm-6    "> </div>
+                        <div class="col-lg-4 col-md-6  col-sm-6     "> <img src="assets/img/bg_images/3.jpg" class="img-fluid herophoto overflow-visible  " alt="m"></div>
+                    </div>
+
+                    <div class="row py-2  ">
+                        <div class="col-lg-2 col-md-4    "> <img src="assets/img/10.jpg" class="img-fluid herophoto d-md-none d-sm-none d-lg-block" alt=""></div>
+                        <div class="col-lg-2 col-md-4   "> <img src="assets/img/10.jpg" class="img-fluid herophoto d-md-none d-sm-none d-lg-block" alt=""></div>
+                        <div class="col-lg-4 col-md-4   "> <img src="assets/img/4.jpg" class="img-fluid herophoto d-md-none d-sm-none d-lg-block" alt=""></div>
+                        <div class="col-lg-4 col-md-4  "> </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    </section>
+
+
+    <div class="container margin_80_55">
+        <div class="main_title_2">
+            <span><em></em></span>
+            <h2>How it works</h2>
+            <p>Spend less money doing it more</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-medal"></i>
+                    <h3>Go save</h3>
+                    <p>You'll save far more with us compared to buying separate attraction tickets.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-help2"></i>
+                    <h3>Go see it all and get free Vouchers</h3>
+                    <p>From unmissable experiences, we’ve got you covered.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-culture"></i>
+                    <h3>Go your way</h3>
+                    <p>You control your sightseeing – mix pre-booked activities with other flexible experiences.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-headphones"></i>
+                    <h3>All to your smartphone </h3>
+                    <p>Just show your pass at each attraction to enter - it's that easy! </p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-credit"></i>
+                    <h3>Secure Payments</h3>
+                    <p>Id mea congue dictas, nec et summo mazim impedit. Vim te audiam impetus interpretaris, cum no alii option, cu sit mazim libris.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <a class="box_feat" href="#0">
+                    <i class="pe-7s-chat"></i>
+                    <h3>Support via Chat</h3>
+                    <p>Id mea congue dictas, nec et summo mazim impedit. Vim te audiam impetus interpretaris, cum no alii option, cu sit mazim libris. </p>
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+
+   
+
+
+    <section class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="main_title_2">
+                    <span><em></em></span>
+                    <h2>Locations</h2>
+                    <p>Check here the available locations</p>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?mykonos');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?mykonos" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Mykonos</a></h4>
+                                <small class="card-meta mb-2">25 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?naxos');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?naxos" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Naxos</a></h4>
+                                <small class="card-meta mb-2">25 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?samos');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?samos" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Samos</a></h4>
+                                <small class="card-meta mb-2">25 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?crete');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?crete" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Crete</a></h4>
+                                <small class="card-meta mb-2">35 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?tech,street');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?tech,street" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Mykonos</a></h4>
+                                <small class="card-meta mb-2">25 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3  col-md-4 col-sm-12">
+                    <div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?greece');">
+                        <img class="card-img d-none" src="https://source.unsplash.com/600x900/?greece" alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?">
+                        <div class="card-img-overlay d-flex flex-column">
+                            <div class="card-body">
+
+                            </div>
+                            <div class="card-footer">
+                                <h4 class="card-title mt-0 "><a class="text-white" herf="#">Ροδος</a></h4>
+                                <small class="card-meta mb-2">25 Advetures</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <div class="container margin_80_55">
+        <section>
+            <div class="newsletter-subscribe  container">
+                <div class="container">
+                    <div class="intro">
+                        <h2 class="text-center newsletter">Subscribe to our Newsletter</h2>
+                        <p class="text-center">Hey… want to save even more?
+                            Sign up today and more save + extrafree Vouchers
+                        </p>
+                    </div>
+                    <form class="form-inline" method="post">
+                        <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Your Email"></div>
+                        <div class="form-group"><select class="form-select" name="" id="">
+
+                                <option value="" disabled selected> Choose your Voucher ValuePass </option>
+                                <option value="2 Vouchers for 14€"> 2 Vouchers for 14€</option>
+                                <option value="3 Vouchers from21€ + extra 1 free Voucher value10€"> 3 Vouchers from21€ + extra 1 free Voucher value10€</option>
+                                <option value="4 Vouchers from28€ +extra 2 freeVoucher value 20€"> 4 Vouchers from28€ +extra 2 freeVoucher value 20€</option>
+                                <option value="5 Vouchers from35€ +extra 2 freeVoucher value 30€"> 5 Vouchers from35€ +extra 2 freeVoucher value 30€</option>
+                                <option value="6 Vouchers from42€ + extra 3 freeVoucher value40€"> 6 Vouchers from42€ + extra 3 freeVoucher value40€</option>
+                                <option value="7 Vouchers from49€ +extra3 freeVoucher value50€"> 7 Vouchers from49€ +extra3 freeVoucher value50€</option>
+
+                            </select></div>
+
+                        <div class="form-group"><button class="btn btn-primary" type="button">Subscribe </button></div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+
+</main>
+<!-- /main -->
+
+<footer>
+    <div class="container margin_60_35">
+        <div class="row">
+            <div class="col-lg-5 col-md-12 pe-5">
+                <!-- <p><img src="img/logo.svg" width="150" height="36" alt=""></p> -->
+                <p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
+                <div class="follow_us">
+                    <ul>
+                        <li>Follow us</li>
+                        <li><a href="#0"><i class="ti-facebook"></i></a></li>
+                        <li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+                        <li><a href="#0"><i class="ti-google"></i></a></li>
+                        <li><a href="#0"><i class="ti-pinterest"></i></a></li>
+                        <li><a href="#0"><i class="ti-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 ms-lg-auto">
+                <h5>Useful links</h5>
+                <ul class="links">
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="register.html">Register</a></li>
+                    <li><a href="blog.html">News &amp; Events</a></li>
+                    <li><a href="contacts.html">Contacts</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5>Contact with Us</h5>
+                <ul class="contacts">
+                    <li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
+                    <li><a href="mailto:info@Panagea.com"><i class="ti-email"></i> info@Panagea.com</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--/row-->
+        <hr>
+        <div class="row">
+            <div class="col-lg-6">
+                <ul id="footer-selector">
+                    <li>
+                        <div class="styled-select" id="lang-selector">
+                            <select>
+                                <option value="English" selected>English</option>
+                                <option value="French">French</option>
+                                <option value="Spanish">Spanish</option>
+                                <option value="Russian">Russian</option>
+                            </select>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="styled-select" id="currency-selector">
+                            <select>
+                                <option value="US Dollars" selected>US Dollars</option>
+                                <option value="Euro">Euro</option>
+                            </select>
+                        </div>
+                    </li>
+                    <!-- <li><img src="img/cards_all.svg" alt=""></li> -->
+                </ul>
+            </div>
+            <div class="col-lg-6">
+                <ul id="additional_links">
+                    <li><a href="#0">Terms and conditions</a></li>
+                    <li><a href="#0">Privacy</a></li>
+                    <li><span>© TechIns</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--/footer-->
+</div>
+<!-- page -->
+
+<!-- Sign In Popup -->
+<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
+    <div class="small-dialog-header">
+        <h3>Sign In</h3>
+    </div>
+    <form>
+        <div class="sign-in-wrapper">
+            <a href="#0" class="social_bt facebook">Login with Facebook</a>
+            <a href="#0" class="social_bt google">Login with Google</a>
+            <div class="divider"><span>Or</span></div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" id="email">
+                <i class="icon_mail_alt"></i>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" name="password" id="password" value="">
+                <i class="icon_lock_alt"></i>
+            </div>
+            <div class="clearfix add_bottom_15">
+                <div class="checkboxes float-start">
+                    <label class="container_check">Remember me
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="float-end mt-1"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
+            </div>
+            <div class="text-center"><input type="submit" value="Log In" class="btn_1 full-width"></div>
+            <div class="text-center">
+                Don’t have an account? <a href="register.html">Sign up</a>
+            </div>
+            <div id="forgot_pw">
+                <div class="form-group">
+                    <label>Please confirm login email below</label>
+                    <input type="email" class="form-control" name="email_forgot" id="email_forgot">
+                    <i class="icon_mail_alt"></i>
+                </div>
+                <p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
+                <div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
+            </div>
+        </div>
+    </form>
+    <!--form -->
+</div>
+<!-- /Sign In Popup -->
+
+<div id="toTop"></div><!-- Back to top button -->
+
+<!-- COMMON SCRIPTS -->
+<script src="assets/js/common_scripts.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/validate.js"></script>
+
+
+
+
+</body>
+
+</html>
