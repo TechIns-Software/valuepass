f7ea188637307ac36d21db64bedc47f25c866c4d
ChristosBaztekas <christosbaztekas@gmail.com>
2022-05-31 20:00:47 +0300

    add connection establishing file

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+//$servername="localhost";
+//$username="root";
+//$password="";
+//$dbname="valuepassdb";
+$servername="localhost";
+$username="u503457546_valuepassdb";
+$password="1234Valuepassbdtest";
+$dbname="u503457546_valuepassdb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
+
+?>
