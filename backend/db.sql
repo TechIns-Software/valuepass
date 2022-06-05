43fb791b221d357bf2ad73c876558ce92aefb7b6
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-05 16:00:32 +0300

    correction to DB declarations

--- backend/db.sql
+++ backend/db.sql
@@ -1,46 +1,48 @@
-CREATE TABLE language (
+-- id of to be started from 1
+CREATE TABLE Language (
     id int NOT NULL,
+    PRIMARY KEY (id),
     language varchar(100),
     icon varchar(50)
-);
+)ENGINE=InnoDB;
 CREATE TABLE Destination (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     image1 varchar (100),
-    image2 varchar (100),
-);
+    image2 varchar (100)
+)ENGINE=InnoDB;
 CREATE TABLE DestinationTranslate (
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     idDestination int NOT NULL,
     FOREIGN KEY (idDestination) REFERENCES Destination(id),
     name varchar (100),
     description text
-);
+)ENGINE=InnoDB;
 CREATE TABLE CategoryVendor (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE CategoryVendorTranslate (
     idCategoryVendor int NOT NULL,
     FOREIGN KEY (idCategoryVendor) REFERENCES CategoryVendor(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     name varchar(100)
-);
+)ENGINE=InnoDB;
 CREATE TABLE PaymentInfoActivity (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE PaymentInfoActivityTranslate (
     idPaymentInfoActivity int NOT NULL,
     FOREIGN KEY (idPaymentInfoActivity) REFERENCES PaymentInfoActivity(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     head varchar(100),
     description varchar (200)
 
-);
+)ENGINE=InnoDB;
 CREATE TABLE Vendor (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
@@ -54,20 +56,20 @@ CREATE TABLE Vendor (
     idCategory int NOT NULL,
     FOREIGN KEY (idCategory) REFERENCES CategoryVendor(id),
     idPaymentInfoActivity int NOT NULL,
-    FOREIGN KEY (idPaymentInfoActivity) REFERENCES PaymentInfoActivity(id),
+    FOREIGN KEY (idPaymentInfoActivity) REFERENCES PaymentInfoActivity(id)
 
-);
+)ENGINE=InnoDB;
 CREATE TABLE VendorTranslate (
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     name varchar(100) NOT NULL ,
     descriptionSmall varchar (80) NOT NUll,
     descriptionBig text NOT NULL,
-    descriptionFull text NOT NULL,
+    descriptionFull text NOT NULL
 
-);
+)ENGINE=InnoDB;
 CREATE TABLE BestOff (
     id int NOT NULL AUTO_INCREMENT, -- not sure needed
     PRIMARY KEY (id),
@@ -75,26 +77,26 @@ CREATE TABLE BestOff (
     FOREIGN KEY (idDestination) REFERENCES Destination(id),
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE BestOffOrder (
     id int NOT NULL AUTO_INCREMENT, -- not sure needed
     PRIMARY KEY (id),
     idBestOff int NOT NULL,
     FOREIGN KEY (idBestOff) REFERENCES BestOff(id),
-    int number --How to return it in software?
-);
-CREATE TABLE LabelsBox(
+    number int -- How to return it in software?
+)ENGINE=InnoDB;
+CREATE TABLE LabelsBox (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    order int NOT NULL
-);
+    orderNumber int NOT NULL
+)ENGINE=InnoDB;
 CREATE TABLE LabelsBoxTranslate (
     idLabelsBox int NOT NULL,
     FOREIGN KEY (idLabelsBox) REFERENCES LabelsBox(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     name varchar(100)
-);
+)ENGINE=InnoDB;
 CREATE TABLE VendorLabelsBox (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
@@ -102,39 +104,39 @@ CREATE TABLE VendorLabelsBox (
     FOREIGN KEY (idVendor) REFERENCES Vendor(id),
     idLabelsBox int NOT NULL,
     FOREIGN KEY (idLabelsBox) REFERENCES LabelsBox(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE VendorImages (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id),
     image varchar(100)
-);
+)ENGINE=InnoDB;
 CREATE TABLE Highlight (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE HighlightTranslate (
     idHighlight int NOT NULL,
     FOREIGN KEY (idHighlight) REFERENCES Highlight(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     name varchar(100)
-);
+)ENGINE=InnoDB;
 CREATE TABLE RatedCategory (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    orderNumber int NOT NULL,
-);
+    orderNumber int NOT NULL
+)ENGINE=InnoDB;
 CREATE TABLE RatedCategoryTranslate (
     idRatedCategory int NOT NULL,
     FOREIGN KEY (idRatedCategory) REFERENCES RatedCategory(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     nameCategory varchar(100) NOT NULL
-);
+)ENGINE=InnoDB;
 CREATE TABLE Rated (
     id int NOT NULL AUTO_INCREMENT, -- not sure needed
     PRIMARY KEY (id),
@@ -143,18 +145,19 @@ CREATE TABLE Rated (
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id),
     stars int NOT NULL
-);
+)ENGINE=InnoDB;
 CREATE TABLE IncludedService (
     id int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
     icon varchar(100)
-);
+)ENGINE=InnoDB;
 CREATE TABLE IncludedServiceTranslate (
     idIncludedService int NOT NULL,
     FOREIGN KEY (idIncludedService) REFERENCES IncludedService(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     name varchar(100) NOT NULL
-);
+)ENGINE=InnoDB;
 CREATE TABLE VendorIncludedService (
     id int NOT NULL AUTO_INCREMENT, -- not sure needed
     PRIMARY KEY (id),
@@ -162,47 +165,47 @@ CREATE TABLE VendorIncludedService (
     FOREIGN KEY (idVendor) REFERENCES Vendor(id),
     idIncludedService int not null,
     FOREIGN KEY (idIncludedService) REFERENCES IncludedService(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE AboutActivity (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE AboutActivityTranslate (
     idAboutActivity int NOT NULL,
     FOREIGN KEY (idAboutActivity) REFERENCES AboutActivity(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
     head varchar (100),
     description varchar (200)
-);
+)ENGINE=InnoDB;
 CREATE TABLE ImportantInformationHead (
     id int NOT NULL,
     PRIMARY KEY (id),
     idVendor int NOT NULL,
     FOREIGN KEY (idVendor) REFERENCES Vendor(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE ImportantInformationDescription (
     id int NOT NULL,
     PRIMARY KEY (id),
     idImportantInformationHead int NOT NULL,
     FOREIGN KEY (idImportantInformationHead) REFERENCES ImportantInformationHead(id)
-);
+)ENGINE=InnoDB;
 CREATE TABLE ImportantInformationHeadTranslate (
     idImportantInformationHead int NOT NULL,
     FOREIGN KEY (idImportantInformationHead) REFERENCES ImportantInformationHead(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
-    name varchar(80),
-);
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
+    name varchar(80)
+)ENGINE=InnoDB;
 CREATE TABLE ImportantInformationDescriptionTranslate (
     idImportantInformationDescription int NOT NULL,
     FOREIGN KEY (idImportantInformationDescription) REFERENCES ImportantInformationDescription(id),
     idLanguage int NOT NULL,
-    FOREIGN KEY (idLanguage) REFERENCES language(id),
-    name varchar(80),
-);
+    FOREIGN KEY (idLanguage) REFERENCES Language(id),
+    name varchar(80)
+)ENGINE=InnoDB;
 
 -- up here have been created DB Scheme
 
@@ -214,7 +217,7 @@ CREATE TABLE ImportantInformationDescriptionTranslate (
 CREATE TABLE Payment(
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-);
+)ENGINE=InnoDB;
 
 
 
@@ -233,7 +236,7 @@ CREATE TABLE Voucher (
     status int NOT NULL, --taken, reserve until payment, free
 --     somehow to know if can be free
 
-);
+)ENGINE=InnoDB;
 
 
 CREATE TABLE VoucherOfPayments (
@@ -243,5 +246,5 @@ CREATE TABLE VoucherOfPayments (
     FOREIGN KEY (idVoucher) REFERENCES Voucher(id),
     idPayment int NOT NULL,
     FOREIGN KEY (idPayment) REFERENCES Payment(id)
-);
+)ENGINE=InnoDB;
 
