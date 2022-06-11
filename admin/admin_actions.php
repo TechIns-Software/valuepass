5fdfda8f56d265bfc511ef792d98507dfa955275
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-11 15:38:34 +0300

    see error

--- admin/admin_actions.php
+++ admin/admin_actions.php
@@ -7,51 +7,54 @@ if (!isset($conn)) {
 }
 include 'admin_library.php';
 session_start();
+try {
+    if (
+        $_POST["action"] == "addlocation" ||
+        $_POST["action"] == "addlabels" ||
+        $_POST["action"] == "addcategory" ||
+        $_POST["action"] == "createbestoff"
+    ) {
+        if ($_POST["action"] == "addlocation") {
+            $numberloc = $_POST["numberoflocations"];
+            $data_langs = $_POST["data"];
 
+            // print_r($data_langs);
+            $table = 'Destination';
+            $last_id = lastInstertedid($conn, $table);
+            addrowDestination($conn, ($last_id + 1));
 
-if (
-    $_POST["action"] == "addlocation" ||
-    $_POST["action"] == "addlabels" ||
-    $_POST["action"] == "addcategory" ||
-    $_POST["action"] == "createbestoff"
-) {
-    if ($_POST["action"] == "addlocation") {
-        $numberloc = $_POST["numberoflocations"];
-        $data_langs = $_POST["data"];
+            foreach ($data_langs as $data_lang) {
+                $id_loc = substr($data_lang[0], -1);
+                AddLocationTranslate($conn, $id_loc, ($last_id + 1), $data_lang[1], $data_lang[3]);
+            }
+        } else   if ($_POST["action"] == "addlabels") {
+            $data_labels = $_POST["data"];
 
-        // print_r($data_langs);
-        $table = 'Destination';
-        $last_id = lastInstertedid($conn, $table);
-        addrowDestination($conn, ($last_id + 1));
+            // print_r($data_labels);
+            $table = 'LabelsBox';
+            $last_id = lastInstertedid($conn, $table);
+            addrowLabelBox($conn, ($last_id + 1));
+            foreach ($data_labels as $data_label) {
+                $id_lang = $data_label[0];
+                AddLabelBoxTranslate($conn, ($last_id + 1), $id_lang, $data_label[1]);
+            }
+        } else  if ($_POST["action"] == "addcategory") {
 
-        foreach ($data_langs as $data_lang) {
-            $id_loc = substr($data_lang[0], -1);
-            AddLocationTranslate($conn, $id_loc, ($last_id + 1), $data_lang[1], $data_lang[3]);
-        }
-    } else   if ($_POST["action"] == "addlabels") {
-        $data_labels = $_POST["data"];
+            $data_labels = $_POST["data"];
 
-        // print_r($data_labels);
-        $table = 'LabelsBox';
-        $last_id = lastInstertedid($conn, $table);
-        addrowLabelBox($conn, ($last_id + 1));
+            // print_r($data_labels);
+            $table = 'CategoryVendor';
+            $last_id = lastInstertedid($conn, $table);
+            addrowCategoryVendor($conn, ($last_id + 1));
 
-        foreach ($data_labels as $data_label) {
-            $id_lang = $data_label[0];
-            AddLabelBoxTranslate($conn, ($last_id + 1), $id_lang, $data_label[1]);
-        }
-    } else  if ($_POST["action"] == "addcategory") {
-
-        $data_labels = $_POST["data"];
-
-        // print_r($data_labels);
-        $table = 'CategoryVendor';
-        $last_id = lastInstertedid($conn, $table);
-        addrowCategoryVendor($conn, ($last_id + 1));
-
-        foreach ($data_labels as $data_label) {
-            $id_lang = $data_label[0];
-            AddCategoryVendorTranslate($conn, ($last_id + 1), $id_lang, $data_label[1]);
+            foreach ($data_labels as $data_label) {
+                $id_lang = $data_label[0];
+                AddCategoryVendorTranslate($conn, ($last_id + 1), $id_lang, $data_label[1]);
+            }
         }
     }
+} catch (Exception $exception) {
+    var_dump($exception);
 }
+
+
