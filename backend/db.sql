89ec41cd25b079cb0057853537e2448dcce8ef39
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-15 22:03:10 +0300

    ..

--- backend/db.sql
+++ backend/db.sql
@@ -383,6 +383,6 @@ INSERT INTO Vendor(idDestination, priceAdult, originalPrice, discount, priceKid,
 VALUES (1, 10, 15, 30, 8, 1, 1);
 INSERT INTO VendorVoucher(idVendor, starterVouchers, existenceVoucher, dateVoucher)
 VALUES
-(3, 10, 10, NOW() + INTERVAL 1 DAY);
+(1, 10, 10, NOW() + INTERVAL 1 DAY);
 INSERT INTO VendorTranslate(idVendor, idLanguage, name, descriptionSmall, descriptionBig, descriptionFull)
-values (3,1, 'name1', '', '',''),(3,2,'name2','','','');
+values (1,1, 'name1', '', '',''),(1,2,'name2','','','');
