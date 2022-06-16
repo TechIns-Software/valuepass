12c698f626166250d7cff438d4ca865552813cb0
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-16 18:42:07 +0300

    add file

---
+++ backend/VendorVoucher.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace ValuePass;
+
+
+class VendorVoucher
+{
+    private int $id;
+    private int $idVendor;
+    private int $starterVouchers;
+    private int $existenceVoucher;
+    private \DateTime $date;
+}
