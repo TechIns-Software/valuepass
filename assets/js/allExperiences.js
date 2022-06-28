5f79552213b7ec036416bdbffb694813f1dea24f
Nick Spanos <nickspanosf1@gmail.com>
2022-06-28 11:19:39 +0300

    small change in js

--- assets/js/allExperiences.js
+++ assets/js/allExperiences.js
@@ -5,10 +5,14 @@ const storesCategory = [];
 const storesIdElement = []
 for (let ccc = 1; ccc <= experiencesElements.length; ccc++) {
     storesCategory.push($(`#exper${ccc}`).attr('data-category'));
-    storesIdElement.push(`exper${ccc}`)
+    // storesIdElement.push(`exper${ccc}`)
 }
 
+experiencesElements.forEach(element => {
+    storesIdElement.push(element.id)
+});
+
 // console.log("This is all category experiences ids");
-console.log(storesCategory);
+console.log(storesIdElement);
 
 
