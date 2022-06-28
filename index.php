84b9da76e42231fac83da7a753c821602fd36f6a
ChristosBaztekas <christosbaztekas@gmail.com>
2022-06-28 23:37:56 +0300

    ..

--- index.php
+++ index.php
@@ -170,8 +170,8 @@ $destinations = getDestinations($conn, $idLanguage);
 		<section class="add_bottom_45">
 			<div class="main_title_2">
 				<span><em></em></span>
-				<h2>1. Destinations</h2>
-				<p>2. Check here the available destinations</p>
+				<h2>Destinations</h2>
+				<p>Check here the available destinations</p>
 			</div>
 
 			<div id="reccomended_adventure" class="owl-carousel owl-theme">
