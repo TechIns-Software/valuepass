324ec61a65401614690f9f4fc94d7ba5cbf1ea96
ChristosBaztekas <christosbaztekas@gmail.com>
2022-09-13 21:00:59 +0300

    change connectio file

--- connection.php
+++ connection.php
@@ -1,19 +1,22 @@
 <?php
-//$servername="localhost";
-//$username="root";
-//$password="";
-//$dbname="valuepassdb";
-$servername="localhost";
-$username="u503457546_valuepassdb";
-$password="1234Valuepassbdtest";
-$dbname="u503457546_valuepassdb";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if(!$conn){
-    die("Connection failed: ".mysqli_connect_error());
+$servername = "localhost";
+$username = "u503457546_dbvaluepass";
+$password = "9lw]G*4+Yb4FqSL3[q|";
+$dbname = "u503457546_dbvaluepass";
+try {
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+} catch (Exception $e) {
+    $username = "root";
+    $password = "";
+    $dbname = "valuepassdb";
+    try {
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+    } catch (Exception $e1) {
+        die("Connection failed");
+    }
 }
+
 
 ?>
