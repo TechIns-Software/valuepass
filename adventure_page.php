e937cdc7a9aefd7e6452f75d0bd133fa558eb209
ChristosBaztekas <christosbaztekas@gmail.com>
2022-09-17 14:12:37 +0300

    remove unnecessary code, vendor page, remove text from slider images

--- adventure_page.php
+++ adventure_page.php
@@ -40,17 +40,9 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
 			<?php
 			$dummyCounter = 0;
 			foreach ($vendor->getImages() as $imagePath) {
-				$dummyArray = [
-					'Unique <strong>Experience </strong>',
-					'<strong>Enjoy</strong> Unforgettable Holidays',
-					'<strong>Top Attractions</strong> to discover'
-				];
 			?>
 				<div class="ls-slide" data-ls="slidedelay: 5000; transition2d:85;">
 					<img src="vendorImages/<?php echo $vendor->getId() . '/' . $imagePath; ?>" class="ls-bg" alt="Slide background">
-					<h3 class="ls-l slide_typo" style="top: 47%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">
-						<?php echo $dummyArray[$dummyCounter % 3] ?>
-					</h3>
 					<!-- <p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
                         Tours - Hotels - Restaurants
                     </p> -->
@@ -58,7 +50,6 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
 
 				</div>
 			<?php
-				$dummyCounter = $dummyCounter + 1;
 			}
 			?>
 
@@ -616,55 +607,6 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
 <!--/footer-->
 </div>
 <!-- page -->
-
-<!-- Sign In Popup -->
-<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
-	<div class="small-dialog-header">
-		<h3>Sign In</h3>
-	</div>
-	<form>
-		<div class="sign-in-wrapper">
-			<a href="#0" class="social_bt facebook">Login with Facebook</a>
-			<a href="#0" class="social_bt google">Login with Google</a>
-			<div class="divider"><span>Or</span></div>
-			<div class="form-group">
-				<label>Email</label>
-				<input type="email" class="form-control" name="email" id="email">
-				<i class="icon_mail_alt"></i>
-			</div>
-			<div class="form-group">
-				<label>Password</label>
-				<input type="password" class="form-control" name="password" id="password" value="">
-				<i class="icon_lock_alt"></i>
-			</div>
-			<div class="clearfix add_bottom_15">
-				<div class="checkboxes float-start">
-					<label class="container_check">Remember me
-						<input type="checkbox">
-						<span class="checkmark"></span>
-					</label>
-				</div>
-				<div class="float-end mt-1"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
-			</div>
-			<div class="text-center"><input type="submit" value="Log In" class="btn_1 full-width"></div>
-			<div class="text-center">
-				Don’t have an account? <a href="#">Sign up</a>
-			</div>
-			<div id="forgot_pw">
-				<div class="form-group">
-					<label>Please confirm login email below</label>
-					<input type="email" class="form-control" name="email_forgot" id="email_forgot">
-					<i class="icon_mail_alt"></i>
-				</div>
-				<p>You will receive an email containing a link allowing you to reset your password to a new
-					preferred one.</p>
-				<div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
-			</div>
-		</div>
-	</form>
-	<!--form -->
-</div>
-<!-- /Sign In Popup -->
 
 <div id="toTop"></div><!-- Back to top button -->
 
