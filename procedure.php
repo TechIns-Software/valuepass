15b3cee3251775a1dada812d714abcdef47a6f4c
ChristosBaztekas <christosbaztekas@gmail.com>
2022-09-18 01:45:32 +0300

    correction

--- procedure.php
+++ procedure.php
@@ -2,19 +2,22 @@
 
 // # We receive the page from user details and if ok redirects to ValuePass Server
 
-if (!isset(
-    $_POST['name'],
-    $_POST['email'],
-    $_POST['promotions']
-
-)) {
-    exit('Bad Request!');
-}
-
+//if (!isset(
+//    $_POST['name'],
+//    $_POST['email'],
+//    $_POST['promotions']
+//
+//)) {
+//    exit('Bad Request!');
+//}
 session_start();
 $cartArray = unserialize($_SESSION['cart']);
+include 'backend/includeClasses.php';
 $cart = new \ValuePass\Cart($cartArray);
 $products = $cart->readyForSendingVendorVoucherData();
+$_POST['name'] = 'Christos';
+$_POST['email'] = 'christosbaztekas@gmail.com';
+$_POST['promotions'] = '1';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,21 +27,18 @@ $products = $cart->readyForSendingVendorVoucherData();
 </head>
 <body>
 <!--TODO: bank link-->
-<form id="myForm" action="https://valuepass.gr/request/payment/caller.php" method="post">
-    <input hidden name="name" value="<?php echo $_POST['name'];?>">
-    <input hidden name="email" value="<?php echo $_POST['email'];?>">
-    <input hidden name="promotions" value="<?php echo $_POST['promotions'];?>">
+<form id="myForm" action="https://valuepass.gr/request/payment/pay.php" method="post">
     <?php
     foreach ($products as $counter=> $product) {
         $idVendorVoucher = $product['idVendorVoucher'];
         $isAdult = $product['isAdult'];
         $numberInfants = $product['numberInfants'];
         ?>
-        <input hidden name="products['<?php echo $counter;?>']"
+        <input hidden name="products['<?php echo $counter;?>'][idVendorVoucher]"
                value="<?php echo $idVendorVoucher;?>">
-        <input hidden name="products['<?php echo $counter;?>']"
+        <input hidden name="products['<?php echo $counter;?>'][isAdult]"
                value="<?php echo $isAdult;?>">
-        <input hidden name="products['<?php echo $counter;?>']"
+        <input hidden name="products['<?php echo $counter;?>'][numberInfants]"
                value="<?php echo $numberInfants;?>">
 
     <?php
