e9229e101518020e9784099ca0a7897eadd1b24e
ChristosBaztekas <christosbaztekas@gmail.com>
2022-09-20 18:59:26 +0300

    change db

--- connection.php
+++ connection.php
@@ -1,16 +1,16 @@
 <?php
 
 $servername = "localhost";
-$username = "u503457546_dbvaluepass";
-$password = "9lw]G*4+Yb4FqSL3[q|";
-$dbname = "u503457546_dbvaluepass";
+$username = "u503457546_demovaluepassv";
+$password = "lCtfo[0NM3V2KV]o*3";
+$dbname = "u503457546_demovaluepassv";
 try {
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 } catch (Exception $e) {
     $username = "root";
     $password = "";
-    $dbname = "valuepassdb";
+    $dbname = "valuepassvm";
     try {
         $conn = mysqli_connect($servername, $username, $password, $dbname);
     } catch (Exception $e1) {
