6b24e4af7385b64abfa6b6518c2ec0495056fe8d
ChristosBaztekas <christosbaztekas@gmail.com>
2022-09-20 19:11:13 +0300

    change db

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "u503457546_demovaluepassvm";
+$username = "u503457546_demovaluepassv";
 $password = "lCtfo[0NM3V2KV]o*3";
-$dbname = "u503457546_demovaluepassvm";
+$dbname = "u503457546_demovaluepassv";
 try {
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
