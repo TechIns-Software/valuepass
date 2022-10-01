1b0cdb4e3dbcf0e4a98bd9c99bd04e49726619ea
Nick Spanos <nickspanosf1@gmail.com>
2022-10-01 14:30:53 +0300

    add terms foreach language

--- index.php
+++ index.php
@@ -304,8 +304,8 @@ getHeader($title, $home, $menu, $languages, $url, $lang_icon, $voucherNumber);
 
 			<div class="col-lg-12">
 				<ul id="additional_links">
-					<li><a><?php echo $menu[10] ?></a></li>
-					<li><a><?php echo $menu[11] ?></a></li>
+					<li><a href="<?php echo $idLanguage == 1 ? 'terms_gr.pdf':'terms_gb.pdf' ?>" target="_blank"><?php echo $menu[10] ?></a></li>
+					<li><a><?php echo $menu[11] ?>gdfgdf</a></li>
 					<li><span>© ValuePass</span></li>
 				</ul>
 			</div>
