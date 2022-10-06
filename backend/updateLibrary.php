9f512fd748c85932219f3344548f073d861f19db
ChristosBaztekas <christosbaztekas@gmail.com>
2022-10-06 14:05:56 +0300

    correction in update basic images

--- backend/updateLibrary.php
+++ backend/updateLibrary.php
@@ -593,7 +593,7 @@ function updateBasicImages($conn, $arr, $tableResponseArray, $table, $nameColumn
     $newVersion = $path = $id = '-1';
     $stmt->bind_param('isi', $newVersion , $path, $id);
     foreach ($arr as $id=>$path) {
-        if (isset($tableResponseArray["$id"][$nameColumn]['version'])) {
+        if (isset($tableResponseArray["$id"]["$nameColumn"]['version'])) {
             $newVersion = $tableResponseArray["$id"]["$nameColumn"]['version'];
         } else {
             $newVersion = 0;
