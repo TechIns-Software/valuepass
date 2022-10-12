1fdbc5e7d4d5fec1e1b5f9eb8f2dc2f8b0e27f3c
Nick Spanos <nickspanosf1@gmail.com>
2022-10-13 02:01:49 +0300

    test viber2

--- includes/header.php
+++ includes/header.php
@@ -98,7 +98,7 @@ function getHeader($title, $home, $menu, $languages, $url, $lang_icon, $voucherN
                     <li><span><a href="#"><?php echo $menu[4] ?></span></a>
                         <ul class="customercare">
                             <li><a href="https://api.whatsapp.com/send/?phone=306931451910&text=Welcome+to+ValuePass%21+How+can+we+help+you%3F+&type=phone_number&app_absent=0"> <img src="assets/icons/whatsapp.png" height="20" width="20" class="img-fluid"> 306931451910</a></li>
-                            <li><a href=" viber://contact?number='%2B306931451910'"> <img src="assets/icons/viber.png" height="20" width="20" class="img-fluid"> +306931451910</a></li>
+                            <li><a href=" https://msng.link/o/?306931451910=vi"> <img src="assets/icons/viber.png" height="20" width="20" class="img-fluid"> +306931451910</a></li>
                             <li><a href="mail:customercare@valuepass.gr" class="icon-email"> customercare@valuepass.gr</a></li>
                             <li><a class="icon-question"> FAQ’s</a></li>
                         </ul>
