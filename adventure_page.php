fd88f5fcfbcec61cb70c85a0710eb8d6de0012ca
Nick Spanos <nickspanosf1@gmail.com>
2023-04-23 17:34:12 +0300

    add pop-ups in adventure_page.php

--- adventure_page.php
+++ adventure_page.php
@@ -92,6 +92,24 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
                                         <b class="p-2 my-0"> <?php echo $vendor->getPaymentInfoActivityDescription(); ?></b>
                                         <ul class="p-2 my-0">
 
+                                            <li><p class="headStyle p-0"><i class="fa fa-arrow-right"
+                                                                            aria-hidden="true"></i>
+                                                    <?php
+                                                    if ($_SESSION["languageId"] == 1){
+                                                        ?>
+                                                        Κάντε Κράτηση εν πλω & <b class="main-blue-color" data-bs-toggle="modal" data-bs-target="#promoCode0"> Πληρώστε αργότερα </b>
+
+                                                        <?php
+                                                    }else{
+                                                        ?>
+                                                        Book onboard & <b class="main-blue-color" data-bs-toggle="modal" data-bs-target="#promoCode0"> Pay later </b>
+                                                    <?php
+                                                    }
+                                                    ?>
+
+                                                </p>
+                                                <small> <?php echo $menu[241]; ?> </small>
+                                            </li>
 
 
                                             <?php
@@ -219,10 +237,10 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
                             <div class="row my-2">
                                 <div class="d-flex justify-content-between">
                                     <div class="p-0 m-0  "><b style=" font-size: 15px; font-weight: 800"> <?php if ($_SESSION['languageId'] == 1) { ?>
-                                                Πληρώστε στην τοποθεσία<br>
+                                               <b class="main-blue-color" data-bs-toggle="modal" data-bs-target="#promoCode0">Πληρώστε αργότερα  </b>  στην τοποθεσία<br>
                                                 της δραστηριότητας
                                             <?php } else { ?>
-                                                Pay at the activity location
+                                                <b class="main-blue-color" data-bs-toggle="modal" data-bs-target="#promoCode0">  Pay later </b> at the activity location
                                             <?php } ?>
                                         </b>
                                     </div>
@@ -569,6 +587,137 @@ $bestOffs = getVendors($conn, $vendor->getIdDestination(), $languageId, true);
             </div>
         </div>
     </div>
+
+    <div class="modal fade " id="promoCode0" tabindex="-1" aria-labelledby="promoCode0" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="promoCode0Title">ValuePass</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <?php
+                if ($_SESSION["languageId"] == 2){
+                ?>
+                <div  class="modal-body">
+                    <h5>1) <b> Find your experience</b></h5>
+                    <p> Choose the experience you want, reserve your spot onboard, and get a
+                        discount on the initial price of the activity by purchasing the VP Voucher </p>
+
+                    <h5>2) <b> Confirmation of the activity through the activity provider</b></h5>
+                    <b> Via email </b>
+
+                    <p class="my-1"> a) Your activity provider will contact you via email with a
+                        Payment link to his unique checkout page.
+                    </p>
+                    <p class="my-1">b) Enter your credit card information to confirm your activity in the provider’s check-out
+                        page.</p>
+                    <br>
+                    <p> There will be <span class="vpicon">  no charge </span> to your credit card as a result of this step. </p>
+                    <p> •	If you fail to show up for the activity after confirming it and you have not canceled it 24 hours in advance, the provider will debit the discounted price of the activity.</p>
+
+                    <h5>3) <b> Pay later at the activity location</b></h5>
+                    <p>The payment for the discounted price will ultimately be made using any method you choose (cash or credit card) at the activity location.</p>
+
+                    <a class="text-info" href="faqs.php"> FAQs/TERMS AND CONDITIONS</a>
+
+                </div>
+                <?php
+                }else{
+                ?>
+
+                    <div  class="modal-body">
+                        <h5>1) <b> Επιλέξτε την εμπειρία σας</b></h5>
+                        <p> Διαλέξτε την εμπειρία που σας ταιριάζει, κάντε κράτηση για τη θέση σας και κερδίστε έκπτωση στην αρχική τιμή της δραστηριότητας αγοράζοντας το VP Voucher εν πλω. </p>
+
+                        <h5>2) <b> Επιβεβαίωση της δραστηριότητας μέσω του παρόχου της</b></h5>
+                        <b> Μέσω Email </b>
+
+                        <p class="my-1"> A) Ο πάροχος της δραστηριότητας θα σας στείλει ένα email με έναν
+                            σύνδεσμο (link) πληρωμής της δικής του σελίδας ολοκλήρωσης κράτησης.
+
+                        </p>
+                        <p class="my-1">B) Συμπληρώστε τα στοιχεία της πιστωτικής σας κάρτας για να
+                            επιβεβαιώσετε τη δραστηριότητα σας.
+                         </p>
+                        <br>
+                        <p> Δεν θα υπάρξει <span class="vpicon">  καμία χρέωση </span> στην πιστωτική σας κάρτα ως αποτέλεσμα αυτού του βήματος. </p>
+                        <p> • Εάν δεν παραβρεθείτε στη δραστηριότητα μετά την επιβεβαίωση της και εφόσον δεν την ακυρώσετε 24 ώρες πριν την έναρξη της, το ποσό θα χρεωθεί αυτόματα από τον πάροχο.</p>
+
+                        <h5>3) <b> Πληρώστε αργότερα στην τοποθεσία της δραστηριότητας</b></h5>
+                        <p> Η πληρωμή της δραστηριότητας γίνεται, χρησιμοποιώντας οποιαδήποτε μέθοδο πληρωμής επιλέξετε (μετρητά ή πιστωτική κάρτα), στην τοποθεσία της δραστηριότητας.</p>
+
+                        <a class="text-info" href="faqs.php">FAQ/ΟΡΟΙ ΚΑΙ ΠΡΟΫΠΟΘΕΣΕΙΣ</a>
+
+                    </div>
+
+                <?php
+                }
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade " id="promoCode1" tabindex="-1" aria-labelledby="questionmodal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="questionmodaltitle">ValuePass</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <?php
+                if ($_SESSION["languageId"] == 2){
+                ?>
+                <div  class="modal-body">
+                    <h5>1) <b> Find your experience</b></h5>
+                    <p> Choose the experience you want, reserve your spot onboard, and get a
+                        discount on the initial price of the activity by purchasing the VP Voucher.
+                    </p>
+
+                    <h5>2) <b> Confirmation of the activity through the activity provider</b></h5>
+
+                    <p class="my-1"> The provider will contact you, simply to confirm your booking
+                        once more and inform you of the remaining steps if needed.
+                    </p>
+
+                    <small> Contact may be made via phone call, message (WhatsApp/Viber), or email. </small>
+                    <small> • You will receive a reminder email from ValuePass Experiences too.</small>
+
+                    <h5>3) <b> Pay later at the activity location</b></h5>
+                    <p>The payment for the discounted price will ultimately be made using any method you choose (cash or credit card) at the activity location.</p>
+
+                    <a class="text-info" href="faqs.php"> FAQs/TERMS AND CONDITIONS</a>
+
+                </div>
+                <?php
+                }else{
+                ?>
+                    <div  class="modal-body">
+                        <h5>1) <b> Επιλέξτε την εμπειρία σας</b></h5>
+                        <p>Διαλέξτε την εμπειρία που σας ταιριάζει, κάντε κράτηση για τη θέση σας και κερδίστε έκπτωση στην αρχική τιμή της δραστηριότητας αγοράζοντας το VP Voucher εν πλω.
+                        </p>
+
+                        <h5>2) <b>Επιβεβαίωση της δραστηριότητας μέσω του παρόχου της</b></h5>
+
+                        <p class="my-1">  Ο πάροχος της δραστηριότητας θα επικοινωνήσει μαζί σας για να επιβεβαιώσει την κράτηση σας εκ νέου και να σας ενημερώσει για περαιτέρω βήματα, στην περίπτωση που αυτά χρειαστούν.
+                        </p>
+
+                        <small> Η επικοινωνία μπορεί να πραγματοποιηθεί μέσω τηλεφωνικής κλήσης, μηνύματος (WhatsApp/ Viber) ή email. </small>
+                        <small> • Θα λάβετε επίσης ένα email υπενθύμισης και από την ValuePass.</small>
+
+                        <h5>3) <b> Πληρώστε αργότερα στην τοποθεσία της δραστηριότητας</b></h5>
+                        <p>Η πληρωμή της δραστηριότητας γίνεται τελικά, χρησιμοποιώντας οποιαδήποτε μέθοδο πληρωμής επιλέξετε (μετρητά, πιστωτική κάρτα), στην τοποθεσία της δραστηριότητας.</p>
+
+                        <a class="text-info" href="faqs.php"> FAQs/ΟΡΟΙ ΚΑΙ ΠΡΟΫΠΟΘΕΣΕΙΣ</a>
+
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
 
 
 </main>
