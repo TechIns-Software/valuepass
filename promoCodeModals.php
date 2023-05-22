a5b34651a87ac6b6ad03ab2a2e0bf17ffa6c90f9
Nick Spanos <nickspanosf1@gmail.com>
2023-05-22 14:03:29 +0300

    promocode page-modal   without session translates

--- promoCodeModals.php
+++ promoCodeModals.php
@@ -1,84 +1,3 @@
-
-<div style="top: 40px" class="modal fade " id="promoCode1" tabindex="-1" aria-labelledby="promoCode0" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
-        <div style="min-height: 40px"></div>
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="promoCode0Title">ValuePass</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <?php
-            if ($_SESSION["languageId"] == 2){
-                ?>
-                <div  class="modal-body">
-
-                    <h2> Book onboard & Pay later</h2>
-                    <p class="my-0"> Reserve your spot onboard and get a discount on the initial price of
-                        the activity via the <b class="vpicon">  VP </b> Voucher. Pay later the discounted price of the activity at its location. </p>
-                    <b class="border-bottom my-2"> How it works</b>
-                    <h5>1) <b> Find your experience</b></h5>
-                    <p> Choose the experience you want, reserve your spot onboard, and get a
-                        discount on the initial price of the activity by purchasing the VP Voucher </p>
-
-                    <h5>2) <b> Confirmation of the activity through the activity provider</b></h5>
-                    <b> Via email </b>
-
-                    <p class="my-1"> a) Your activity provider will contact you via email with a
-                        Payment link to his unique checkout page.
-                    </p>
-                    <p class="my-1">b) Enter your credit card information to confirm your activity in the provider’s check-out
-                        page.</p>
-                    <br>
-                    <p> There will be <span class="vpicon">  no charge </span> to your credit card as a result of this step. </p>
-                    <p> •	If you fail to show up for the activity after confirming it and you have not canceled it 24 hours in advance, the provider will debit the discounted price of the activity.</p>
-
-                    <h5>3) <b> Pay later at the activity location</b></h5>
-                    <p>The payment for the discounted price will ultimately be made using any method you choose (cash or credit card) at the activity location.</p>
-
-                    <a class="text-info" href="faqs.php"> FAQs/TERMS AND CONDITIONS</a>
-
-                </div>
-                <?php
-            }else{
-                ?>
-
-                <div  class="modal-body">
-
-                    <h2> Κάντε Κράτηση εν πλω & Πληρώστε αργότερα</h2>
-                    <p class="my-0"> Κρατήστε τη θέση σας εν πλω και κερδίστε έκπτωση στην αρχική τιμή της
-                        δραστηριότητας μέσω του <b class="vpicon">  VP </b> Voucher. Πληρώστε αργότερα την τιμή της δραστηριότητας σας με έκπτωση στην τοποθεσία της. </p>
-                    <b class="border-bottom my-2"> Πώς λειτουργεί </b>
-                    <h5>1) <b> Επιλέξτε την εμπειρία σας</b></h5>
-                    <p> Διαλέξτε την εμπειρία που σας ταιριάζει, κάντε κράτηση για τη θέση σας και κερδίστε έκπτωση στην αρχική τιμή της δραστηριότητας αγοράζοντας το VP Voucher εν πλω. </p>
-
-                    <h5>2) <b> Επιβεβαίωση της δραστηριότητας μέσω του παρόχου της</b></h5>
-                    <b> Μέσω Email </b>
-
-                    <p class="my-1"> A) Ο πάροχος της δραστηριότητας θα σας στείλει ένα email με έναν
-                        σύνδεσμο (link) πληρωμής της δικής του σελίδας ολοκλήρωσης κράτησης.
-
-                    </p>
-                    <p class="my-1">B) Συμπληρώστε τα στοιχεία της πιστωτικής σας κάρτας για να
-                        επιβεβαιώσετε τη δραστηριότητα σας.
-                    </p>
-                    <br>
-                    <p> Δεν θα υπάρξει <span class="vpicon">  καμία χρέωση </span> στην πιστωτική σας κάρτα ως αποτέλεσμα αυτού του βήματος. </p>
-                    <p> • Εάν δεν παραβρεθείτε στη δραστηριότητα μετά την επιβεβαίωση της και εφόσον δεν την ακυρώσετε 24 ώρες πριν την έναρξη της, το ποσό θα χρεωθεί αυτόματα από τον πάροχο.</p>
-
-                    <h5>3) <b> Πληρώστε αργότερα στην τοποθεσία της δραστηριότητας</b></h5>
-                    <p> Η πληρωμή της δραστηριότητας γίνεται, χρησιμοποιώντας οποιαδήποτε μέθοδο πληρωμής επιλέξετε (μετρητά ή πιστωτική κάρτα), στην τοποθεσία της δραστηριότητας.</p>
-
-                    <a class="text-info" href="faqs.php">FAQ/ΟΡΟΙ ΚΑΙ ΠΡΟΫΠΟΘΕΣΕΙΣ</a>
-
-                </div>
-
-                <?php
-            }
-            ?>
-
-        </div>
-    </div>
-</div>
 <div style="top: 40px" class="modal fade " id="promoCode0" tabindex="-1" aria-labelledby="questionmodal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-xl">
         <div class="modal-content">
@@ -86,68 +5,68 @@
                 <h1 class="modal-title fs-5" id="questionmodaltitle">ValuePass</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <?php
-            if ($_SESSION["languageId"] == 2){
-                ?>
-                <div  class="modal-body">
-                    <h2> Book onboard & Pay later</h2>
-                    <p class="my-0"> Reserve your spot onboard and get a discount on the initial price of
-                        the activity via the <b class="vpicon">  VP </b> Voucher. Pay later the discounted price of the activity at its location. </p>
-                    <h5 class="border-bottom my-2"> How it works</h5>
+            <div  class="modal-body">
+                <h2><?php echo $menu[305]?> </h2>
+                <p> <?php echo $menu[306]?></p>
+                <h5 class="border-bottom my-2"><?php echo $menu[307]?></h5>
 
 
-                    <h5>1) <b> Find your experience</b></h5>
-                    <p> Choose the experience you want, reserve your spot onboard, and get a
-                        discount on the initial price of the activity by purchasing the VP Voucher.
-                    </p>
+                <h5>1) <b> <?php echo $menu[308]?></b></h5>
+                <p> <?php echo $menu[309]?></p>
 
-                    <h5>2) <b> Confirmation of the activity through the activity provider</b></h5>
+                <h5>2) <b> <?php echo $menu[310]?></b></h5>
 
-                    <p class="my-1"> The provider will contact you, simply to confirm your booking
-                        once more and inform you of the remaining steps if needed.
-                    </p>
+                <p class="my-1"> <?php echo $menu[319]?>
+                </p>
 
-                    <small> Contact may be made via phone call, message (WhatsApp/Viber), or email. </small>
-                    <small> • You will receive a reminder email from ValuePass Experiences too.</small>
+                <small> <?php echo $menu[320]?> </small>
+                <small> • <?php echo $menu[321]?></small>
 
-                    <h5>3) <b> Pay later at the activity location</b></h5>
-                    <p>The payment for the discounted price will ultimately be made using any method you choose (cash or credit card) at the activity location.</p>
+                <h5>3) <b> <?php echo $menu[316]?></b></h5>
+                <p><?php echo $menu[317]?></p>
 
-                    <a class="text-info" href="faqs.php"> FAQs/TERMS AND CONDITIONS</a>
+                <a class="text-info" href="faqs.php"> <?php echo $menu[318]?></a>
 
-                </div>
-                <?php
-            }else{
-                ?>
-                <div  class="modal-body">
+            </div>
+        </div>
+    </div>
+</div>
+<div style="top: 40px" class="modal fade " id="promoCode1" tabindex="-1" aria-labelledby="promoCode0"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div style="min-height: 40px"></div>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="promoCode0Title">ValuePass</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
 
-                    <h3> Κάντε Κράτηση εν πλω & Πληρώστε αργότερα</h3>
-                    <p class="my-0"  > Κρατήστε τη θέση σας εν πλω και κερδίστε έκπτωση στην αρχική τιμή της
-                        δραστηριότητας μέσω του <b class="vpicon">  VP </b> Voucher. Πληρώστε αργότερα την τιμή της δραστηριότητας σας με έκπτωση στην τοποθεσία της. </p>
-                    <h5 class="border-bottom my-2"> Πώς λειτουργεί </h5>
+            <div class="modal-body">
 
+                <h2> <?php echo $menu[305]; ?></h2>
+                <p class="my-0">
+                    <?php echo $menu[306]; ?>
+                </p>
+                <b class="border-bottom my-2">    <?php echo $menu[307]; ?></b>
+                <h5>1) <b> <?php echo $menu[308]; ?></b></h5>
+                <p>
+                    <?php echo $menu[309]; ?>
+                </p>
 
-                    <h5>1) <b> Επιλέξτε την εμπειρία σας</b></h5>
-                    <p>Διαλέξτε την εμπειρία που σας ταιριάζει, κάντε κράτηση για τη θέση σας και κερδίστε έκπτωση στην αρχική τιμή της δραστηριότητας αγοράζοντας το VP Voucher εν πλω.
-                    </p>
+                <h5>2) <b> <?php echo $menu[310]; ?></b></h5>
+                <b>   <?php echo $menu[311]; ?> </b>
 
-                    <h5>2) <b>Επιβεβαίωση της δραστηριότητας μέσω του παρόχου της</b></h5>
-
-                    <p class="my-1">  Ο πάροχος της δραστηριότητας θα επικοινωνήσει μαζί σας για να επιβεβαιώσει την κράτηση σας εκ νέου και να σας ενημερώσει για περαιτέρω βήματα, στην περίπτωση που αυτά χρειαστούν.
-                    </p>
-
-                    <small> Η επικοινωνία μπορεί να πραγματοποιηθεί μέσω τηλεφωνικής κλήσης, μηνύματος (WhatsApp/ Viber) ή email. </small>
-                    <small> • Θα λάβετε επίσης ένα email υπενθύμισης και από την ValuePass.</small>
-
-                    <h5>3) <b> Πληρώστε αργότερα στην τοποθεσία της δραστηριότητας</b></h5>
-                    <p>Η πληρωμή της δραστηριότητας γίνεται τελικά, χρησιμοποιώντας οποιαδήποτε μέθοδο πληρωμής επιλέξετε (μετρητά, πιστωτική κάρτα), στην τοποθεσία της δραστηριότητας.</p>
-
-                    <a class="text-info" href="faqs.php"> FAQs/ΟΡΟΙ ΚΑΙ ΠΡΟΫΠΟΘΕΣΕΙΣ</a>
-
-                </div>
-                <?php
-            }
-            ?>
+                <p class="my-1"> A) <?php echo $menu[312]; ?>   </p>
+                <p class="my-1">B) <?php echo $menu[313]; ?> </p>
+                <br>
+                <p>
+                    <?php echo $menu[314]; ?>
+                </p>
+                <p> • <?php echo $menu[315]; ?>    </p>
+                <h5>3) <b>  <?php echo $menu[316]; ?> </b></h5>
+                <p><?php echo $menu[317]; ?></p>
+                <a class="text-info" href="faqs.php"> <?php echo $menu[318]; ?></a>
+            </div>
         </div>
     </div>
 </div>
