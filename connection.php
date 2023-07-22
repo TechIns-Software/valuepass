b6a0e9dccdb9312af720ae3cc3590883b23ce7b3
ChristosBaztekas <christosbaztekas@gmail.com>
2023-07-22 19:25:04 +0300

    add db for preview

--- connection.php
+++ connection.php
@@ -1,21 +1,30 @@
 <?php
 
 $servername = "localhost";
-$username = "u503457546_demovaluepassv";
-$password = "lCtfo[0NM3V2KV]o*3";
-$dbname = "u503457546_demovaluepassv";
+$username = "root";
+$password = "";
+$dbname = "valuepass_vm";
 try {
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 } catch (Exception $e) {
-    $username = "root";
-    $password = "";
-    $dbname = "valuepass_vm";
     try {
+        if (str_contains($_SERVER['REQUEST_URI'], 'demo')) {
+            $username = "u503457546_demovaluepassv";
+            $password = "lCtfo[0NM3V2KV]o*3";
+            $dbname = "u503457546_demovaluepassv";
+        } else {
+            $username = "u503457546_vpvmpreview";
+            $password = "c6!8rLgYfJ4yhIfieF^lJ!";
+            $dbname = "u503457546_vpvmpreview";
+        }
         $conn = mysqli_connect($servername, $username, $password, $dbname);
-    } catch (Exception $e1) {
+
+    } catch (Exception $e) {
         die("Connection failed");
+
     }
+
 }
 
 
