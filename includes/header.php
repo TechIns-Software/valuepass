43630d341fac18777c0685a8bd85289475d48112
ChristosBaztekas <christosbaztekas@gmail.com>
2023-08-22 08:25:09 +0300

    add matomo script

--- includes/header.php
+++ includes/header.php
@@ -57,6 +57,21 @@ function getHeader($title, $home, $menu, $languages, $url, $lang_icon, $voucherN
 
     <!-- Modernizr -->
     <script src="assets/js/modernizr.js"></script>
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="https://valuepass.attica-group.net/matomo/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
 
 </head>
 <body>
